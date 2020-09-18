0d4612d8a6f0d604d672fbd298c70204017900c5
DESKTOP-325D3AA\jade <chetsada_changphanit@cmu.ac.th>
2020-09-18 18:41:39 +0700

    Commit3

---
+++ ScensManagementTmp/Assets/Scenes/SingletonGameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonGameManager : Singleton<SingletonGameManager>
+{
+    protected SingletonGameManager() { }
+   
+    public string ClassName { get; } = "SingletonGameManager";
+    public string playername { get; set; } = "Jade";
+    public int GameScore { get; set; } = 0;
+    
+}
